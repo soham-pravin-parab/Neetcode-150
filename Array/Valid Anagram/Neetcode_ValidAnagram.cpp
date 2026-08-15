b0bb0e2999928dbae894b51eea92e981bae18e13
soham-pravin-parab <sohamparabmail@gmail.com>
2026-08-15 20:46:21 +0530

    Add files via upload

---
+++ Array/Valid Anagram/Neetcode_ValidAnagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        vector<int>count(26,0);
+        if(s.size()!=t.size()){
+            return false ;
+        }
+        for(int i=0;i<s.size();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+
+        }
+        for(int n:count ){
+            if(n!=0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
